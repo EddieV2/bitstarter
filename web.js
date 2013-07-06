6243392c41f5350a9fa2508c5fa672c8cd282bc7
Eddie Vartanessian <e_vartanessian@live.com>
2013-07-06 19:27:42 -0400

    Updated web.js

--- web.js
+++ web.js
@@ -1,11 +1,10 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-	
-var buffer = new buffer ("Hello World from index.html", "utf-8");
 
 app.get('/', function(request, response) {
 	var fs = require('fs');
+	var buffer = new buffer (256);
     fs.readFileSync("index.html");
     console.log(buffer.toString('utf-8'));
  });
