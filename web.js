1074f2e5e24492fbd14eed5ccd0f203c002c41fe
Eddie Vartanessian <e_vartanessian@live.com>
2013-07-06 19:57:25 -0400

    Updated web.js

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 var fs = require('fs');
 var index =  fs.readFileSync('index.html', 'utf-8');
 app.get('/', function(request, response) {
-    response.send('hello');
+    response.send(index);
  });
 
 var port = process.env.PORT || 5000;
