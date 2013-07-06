63d33c0fc9c1acd8089e349e42b9b78702d7ac28
Eddie Vartanessian <e_vartanessian@live.com>
2013-07-06 18:52:41 -0400

    Updated web.js

--- web.js
+++ web.js
@@ -1,10 +1,12 @@
 var express = require('express');
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 var fs = require('fs');
 var buffer = new buffer ("Hello World from index.html", "utf-8");
-  response.send(buffer.toString('utf-8', fs.readFileSync("index.html")))});
+    fs.readFileSync("index.html");
+    console.log(buffer.toString('utf-8'));
  });
 
 var port = process.env.PORT || 5000;
